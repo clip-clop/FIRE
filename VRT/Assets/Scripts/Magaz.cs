0fdddb96f6cb0cd0cc93b519454abecf86c8726e
Maxim20051 <73996350+Maxim20051@users.noreply.github.com>
2021-04-17 16:54:23 +0400

    Add first verion of project

---
+++ VRT/Assets/Scripts/Magaz.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Magaz : MonoBehaviour
+{
+    public Bonding1 bonding1;
+    public int bulletcount;
+    public Text textShowbulletByMagaz;
+    public bool transmissionData;
+    public bool catchData;
+    [SerializeField]
+    bool wasConnected;
+    private void Start()
+    {
+        wasConnected = false;
+        transmissionData = false;
+        catchData = false;
+    }
+    private void Update()
+    {
+        textShowbulletByMagaz.text = bulletcount.ToString();
+        if (bonding1.isbonding == true  || bonding1.interactable.attachedToHand == null)
+        {
+            textShowbulletByMagaz.enabled = false;
+            if(bonding1.isbonding == true && transmissionData == false)
+            {
+                wasConnected = true;
+                bonding1.weaponsIndex.weaponsTag.GetComponent<FireActions>().bulletCountOnMainbody = bulletcount;
+                transmissionData = true;
+            }
+        }
+        else if (bonding1.isbonding == false || bonding1.interactable.attachedToHand != null)
+        {
+            textShowbulletByMagaz.enabled = true;
+            if(bonding1.isbonding == false && bonding1.weaponsIndex != null && wasConnected == true)
+            {
+                wasConnected = false;
+                catchData = true;
+                if(catchData == true)
+                {
+                    bulletcount = bonding1.weaponsIndex.weaponsTag.GetComponent<FireActions>().bulletCountOnMainbody;
+                    catchData = false;
+                }
+                StartCoroutine(ResettingBulletCountData());
+                transmissionData = false;
+            }
+        }
+    }
+    IEnumerator ResettingBulletCountData()
+    {
+        yield return new WaitForSeconds(0.01f);  
+        if(bonding1.weaponsIndex != null)
+        bonding1.weaponsIndex.weaponsTag.GetComponent<FireActions>().bulletCountOnMainbody = 0;
+
+        bonding1.weaponsIndex = null;
+    }
+}
---
+++ VRT/Assets/Scripts/UnBonding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class UnBonding : MonoBehaviour
+{
+    Bonding1 bonding1;
+   
+    private void Start()
+    {
+        bonding1 = GetComponent<Bonding1>();
+    }
+    void Update()
+    {
+       if(bonding1.interactable.attachedToHand != null && bonding1.weaponsIndex != null)
+       {
+            bonding1.weaponsIndex.weaponsTag.GetComponent<MainModel>().scoreOfbondig[bonding1.indexOfPiece - 1] = false;
+            bonding1.isbonding = false;
+       }
+      
+    }
+
+}
