b8143de646f3cc12ccc6408b75b9cac35d2a2fb1
Maxim20051 <73996350+Maxim20051@users.noreply.github.com>
2021-04-24 16:39:53 +0400

    Update all project. Again.

---
+++ ITIR/Assets/Scripts/PartsOption.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsOption : MonoBehaviour
+{
+    public Bonding bonding;
+    Rigidbody Irigidbody;
+    private void Start()
+    {
+        Irigidbody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if(bonding.wasBonded == true)
+        {
+            Irigidbody.isKinematic = true;
+            transform.position = bonding.partsPosition.transform.position;
+            transform.rotation = bonding.partsPosition.transform.rotation;
+            
+        }
+    }
+}
---
+++ ITIR/Assets/Scripts/PointsOfMainDetale.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsOfMainDetale : MonoBehaviour
+{
+    public int indexOfPoint;//индекс точки, для проверки 
+    public string nameOfDetale;
+    public GameObject mainDetale;//главная деталь 
+
+    public int chainOfConnection;//показывает на какой сейчас стадии скрепление
+    public GameObject[] pointsPosition;//кординатные точки при скреплении  
+
+    public PointsOfMainDetale pointsOfMainDetale;
+    
+
+   
+    void Update()
+    {
+        if(indexOfPoint != 1)
+        {
+            chainOfConnection = pointsOfMainDetale.chainOfConnection;
+        }
+    }
+}
